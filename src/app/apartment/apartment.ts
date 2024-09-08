7088fb4dac2a55e0e339a9137111c7b293c7897f
alidauda <alidauda14@gmail.com>
2024-09-08 18:24:35 +0100

    Add authentication and filter properties by user's gender

--- src/app/apartment/apartment.ts
+++ src/app/apartment/apartment.ts
@@ -1,8 +1,26 @@
 "use server";
+import { validateRequest } from "@/utils/auth";
 import prisma from "@/utils/db";
 
 export async function get_all_apartment() {
- 
+  const session = await validateRequest();
+  if (session.user) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+    });
+    return prisma.property.findMany({
+      where: {
+        gender: user?.gender,
+      },
+
+      include: {
+        images: true,
+        rooms: true,
+      },
+    });
+  }
   return prisma.property.findMany({
     include: {
       images: true,
